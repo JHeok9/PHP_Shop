88301220590e628d9af0534a747e214352ba8d6c
JHeok <skdhd999@gmail.com>
2024-06-20 17:03:56 +0900

    반환값 장바구니 번호 추가

--- cart/get_cart.php
+++ cart/get_cart.php
@@ -4,13 +4,14 @@ header('Content-Type: application/json'); // 응답 헤더를 JSON 형식으로 
 
 $user_seq = mysqli_real_escape_string($conn, $_GET['user']);
 
-$sql = "select i.* from cart c left join item i on c.item_seq = i.seq where c.user_seq = $user_seq";
+$sql = "select c.seq as cart_id, i.* from cart c left join item i on c.item_seq = i.seq where c.user_seq = $user_seq";
 
 try{
     $result = mysqli_query($conn, $sql);
     $cart_list = array();
     while($row = mysqli_fetch_assoc($result)){
         $item = array(
+            'cart_id' => $row['cart_id'],
             'seq' => $row['seq'],
             'name' => $row['name'],
             'img1' => $row['img1'],
