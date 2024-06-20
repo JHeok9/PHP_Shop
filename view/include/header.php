415693d403606f9ae669a9c481fb71ede3061e01
JHeok <skdhd999@gmail.com>
2024-06-20 16:31:34 +0900

    commit

--- view/include/header.php
+++ view/include/header.php
@@ -12,10 +12,10 @@
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="products.php">Products</a></li>
+                <!-- <li><a href="products.php">Products</a></li> -->
                 <li><a href="cart.php">Cart</a></li>
                 <li><a href="mypage.php">MyPage</a></li>
-                <li><a href="login.php">Login</a></li>
+                <li id="login-logout"><a href="login.php">Login</a></li>
             </ul>
         </nav>
     </header>
