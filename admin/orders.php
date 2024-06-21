fe15200dd73033290ee586614c6e737b1f00d21e
JHeok <skdhd999@gmail.com>
2024-06-21 15:31:05 +0900

    주문현황

---
+++ admin/orders.php
@@ -0,0 +1,22 @@
+<?php 
+require_once "../common/dbconn.php";
+header('Content-Type: application/json'); // 응답 헤더를 JSON 형식으로 설정
+
+$sql = "select o.seq, u.id, i.name, o.price, a.addr_num, a.addr, a.addr_detail, o.order_date, o.status
+          from orders o left join user u
+            on o.user_seq = u.seq left join order_detail od
+            on o.seq = od.order_seq left join item i
+            on od.item_seq = i.seq left join address a
+            on o.addr_seq = a.seq
+        order by o.order_date desc";
+
+$result = mysqli_query($conn,$sql);
+
+$orders = array();
+
+while($row = mysqli_fetch_assoc($result)){
+    $orders[] = $row;
+}
+
+echo json_encode($orders);
+?>
---
+++ view/admin.php
@@ -0,0 +1,43 @@
+<?php
+require_once "include/header.php";
+?>
+   <main>
+       <ul>
+        <li>
+            <a href="admin.php">주문현황</a>
+        </li>
+        <li>
+            <a href="addItem.php">상품등록</a>
+        </li>
+        <li>
+            <a href="modifyItem.php">상품수정</a>
+        </li>
+    </ul>
+        <div id="product-registration-container" class="content-container">
+            <h2>주문 현황</h2>
+            <table border="1">
+                <colgroup>
+                    <col width="5%">
+                    <col width="10%">
+                    <col width="20%">
+                    <col width="10%">
+                    <col width="30%">
+                    <col width="15%">
+                    <col width="15%">
+                </colgroup>
+                <thead>
+                    <th>주문번호</th>
+                    <th>유저ID</th>
+                    <th>상품명</th>
+                    <th>주문금액</th>
+                    <th>배송주소</th>
+                    <th>주문날짜</th>
+                    <th>주문현황</th>
+                </thead>
+                <tbody id="admin-orders">
+                    
+                </tbody>
+            </table>
+        </div>
+    </main>
+<?php require_once "include/footer.php"; ?>
