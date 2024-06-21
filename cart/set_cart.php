d39b75bc4907537dcb561e1e7f716397e2206591
JHeok <skdhd999@gmail.com>
2024-06-21 09:51:12 +0900

    commit

--- cart/set_cart.php
+++ cart/set_cart.php
@@ -2,10 +2,11 @@
 require_once "../common/dbconn.php";
 header('Content-Type: application/json'); // 응답 헤더를 JSON 형식으로 설정
 
-$user_seq = mysqli_real_escape_string($conn, $_GET['user']);
-$item_seq = mysqli_real_escape_string($conn, $_GET['item']);
+// JSON 데이터 읽기
+$input = file_get_contents('php://input');
+$data = json_decode($input, true);
 
-$sql = "insert into cart (user_seq, item_seq) values($user_seq, $item_seq)";
+$sql = "insert into cart (user_seq, item_seq) values({$data['user_id']}, {$data['item_id']})";
 
 try{
     $result = mysqli_query($conn, $sql);
--- view/include/footer.php
+++ view/include/footer.php
@@ -1,6 +1,6 @@
 <footer>
         <p>&copy; 2024 My Shop. All rights reserved.</p>
     </footer>
-    <script src="js/script.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
